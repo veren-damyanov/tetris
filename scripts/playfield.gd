3240c0a94303f68af1c73b1f035ae54d6faea72a
Veren Damyanov <veren.damyanov@protonmail.com>
2023-06-28 23:04:46 +0300

    Replace tabs with spaces in the scripts (yeah..)

--- scripts/playfield.gd
+++ scripts/playfield.gd
@@ -6,98 +6,97 @@ var elapsed      # time since last move
 var active_shape # currently active (falling) shape
 
 var shape_map = {
-	'I': [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
-	'J': [Vector2(-1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	'L': [Vector2(1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	'O': [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
-	'S': [Vector2(0, -1), Vector2(1, -1), Vector2(1, 0), Vector2(0, 0)],
-	'T': [Vector2(0, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
-	'Z': [Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0)],
+    'I': [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
+    'J': [Vector2(-1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+    'L': [Vector2(1, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+    'O': [Vector2(0, 0), Vector2(1, 0), Vector2(0, 1), Vector2(1, 1)],
+    'S': [Vector2(0, -1), Vector2(1, -1), Vector2(1, 0), Vector2(0, 0)],
+    'T': [Vector2(0, -1), Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0)],
+    'Z': [Vector2(-1, -1), Vector2(0, -1), Vector2(0, 0), Vector2(1, 0)],
 }
 
 func _ready():
-	self.globals = get_node("/root/globals")
-	self.elapsed = 0
-	self._init_matrix()
-	self.active_shape = self._new_active_shape()
-	self.add_child(self.active_shape)
+    self.globals = get_node("/root/globals")
+    self.elapsed = 0
+    self._init_matrix()
+    self.active_shape = self._new_active_shape()
+    self.add_child(self.active_shape)
 
 func _process(delta):
-	self.get_node('Label').set_text("FPS %d" % Engine.get_frames_per_second())
-	elapsed += delta
-	
-#	var left = Input.is_action_pressed("left")
-#	var right = Input.is_action_pressed("right")
-#	var space = Input.is_action_pressed("space")
-#
-#	if left and not right:
-#		pass
-#	elif right and not left:
-#		pass
+    self.get_node('Label').set_text("FPS %d" % Engine.get_frames_per_second())
+    elapsed += delta
 
-	if elapsed >= 0.2:
-		elapsed = 0
-		if self._is_active_shape_movable(0, 1):
-			self._move_active_shape(0, 1)
-		else:
-			self._deactivate_current_shape()
-			self.active_shape = self._new_active_shape()
-			self.add_child(self.active_shape)
+#    var left = Input.is_action_pressed("left")
+#    var right = Input.is_action_pressed("right")
+#    var space = Input.is_action_pressed("space")
+#
+#    if left and not right:
+#        pass
+#    elif right and not left:
+#        pass
+
+    if elapsed >= 0.2:
+        elapsed = 0
+        if self._is_active_shape_movable(0, 1):
+            self._move_active_shape(0, 1)
+        else:
+            self._deactivate_current_shape()
+            self.active_shape = self._new_active_shape()
+            self.add_child(self.active_shape)
 
 func _new_active_shape():
-	var shape = self._new_shape()
-	var x = globals.TILE_SIZE / 2 + globals.OFFSET_X + globals.TILE_SIZE * 4
-	var y = globals.TILE_SIZE / 2 + globals.OFFSET_Y + globals.TILE_SIZE * -2
-	shape.set_position(Vector2(x, y))
-	shape.set_tpos(Vector2(4, 0))  # FIXME: this position is temporary, should be changed (also above ^^) 
-	return shape
+    var shape = self._new_shape()
+    var x = globals.TILE_SIZE / 2 + globals.OFFSET_X + globals.TILE_SIZE * 4
+    var y = globals.TILE_SIZE / 2 + globals.OFFSET_Y + globals.TILE_SIZE * -2
+    shape.set_position(Vector2(x, y))
+    shape.set_tpos(Vector2(4, 0))  # FIXME: this position is temporary, should be changed (also above ^^)
+    return shape
 
 func _deactivate_current_shape():
-	var tpos = self.active_shape.get_tpos()
-	var coords = self.active_shape.get_coords()
-	for v in coords:
-		var x = tpos.x + v.x
-		var y = tpos.y + v.y
-		self.matrix[x][y] = 1
-	
+    var tpos = self.active_shape.get_tpos()
+    var coords = self.active_shape.get_coords()
+    for v in coords:
+        var x = tpos.x + v.x
+        var y = tpos.y + v.y
+        self.matrix[x][y] = 1
 
 func _is_active_shape_movable(dx, dy):
-	var tpos = self.active_shape.get_tpos()
-	var coords = self.active_shape.get_coords()
-	for v in coords:
-		var x = tpos.x + v.x + dx
-		var y = tpos.y + v.y + dy
-		if self.matrix[x][y] == 1:
-			return false
-	return true
+    var tpos = self.active_shape.get_tpos()
+    var coords = self.active_shape.get_coords()
+    for v in coords:
+        var x = tpos.x + v.x + dx
+        var y = tpos.y + v.y + dy
+        if self.matrix[x][y] == 1:
+            return false
+    return true
 
 func _move_active_shape(dx, dy):
-	var tpos = self.active_shape.get_tpos()
-	tpos.x += dx
-	tpos.y += dy
-	self.active_shape.set_tpos(tpos)
-	var x = globals.TILE_SIZE / 2 + globals.OFFSET_X + globals.TILE_SIZE * tpos.x
-	var y = globals.TILE_SIZE / 2 + globals.OFFSET_Y + globals.TILE_SIZE * tpos.y
-	self.active_shape.set_position(Vector2(x, y))
+    var tpos = self.active_shape.get_tpos()
+    tpos.x += dx
+    tpos.y += dy
+    self.active_shape.set_tpos(tpos)
+    var x = globals.TILE_SIZE / 2 + globals.OFFSET_X + globals.TILE_SIZE * tpos.x
+    var y = globals.TILE_SIZE / 2 + globals.OFFSET_Y + globals.TILE_SIZE * tpos.y
+    self.active_shape.set_position(Vector2(x, y))
 
 func _init_matrix():
-	self.matrix = []
-	for i in range(self.globals.FIELD_X+2):
-		self.matrix.append([])
-		for j in range(self.globals.FIELD_Y):
-			if i == 0 or i == self.globals.FIELD_X+1:
-				self.matrix[i].append(1)
-			else:
-				self.matrix[i].append(0)
-		self.matrix[i].append(1)
-	# print(self.matrix)
+    self.matrix = []
+    for i in range(self.globals.FIELD_X+2):
+        self.matrix.append([])
+        for j in range(self.globals.FIELD_Y):
+            if i == 0 or i == self.globals.FIELD_X+1:
+                self.matrix[i].append(1)
+            else:
+                self.matrix[i].append(0)
+        self.matrix[i].append(1)
+    # print(self.matrix)
 
 func _randomize():
-	var options = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
-	return options[randi() % options.size()]
+    var options = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
+    return options[randi() % options.size()]
 
 func _new_shape():
-	var shape = preload("res://shape.tscn").instantiate()
-	var type = self._randomize()
-	shape.init(type, shape_map[type])
-	return shape
+    var shape = preload("res://shape.tscn").instantiate()
+    var type = self._randomize()
+    shape.init(type, shape_map[type])
+    return shape
--- scripts/shape.gd
+++ scripts/shape.gd
@@ -7,41 +7,41 @@ var size   # used for rotation
 var tpos  # position within field matrix
 
 func _ready():
-	globals = get_node("/root/globals")
-	var blocks = self.get_children()
-	var color = self._pick_random_color()
-	for i in range(4):
-		blocks[i].position = coords[i] * globals.TILE_SIZE
-		blocks[i].get_node('Sprite2D').set_modulate(color)
+    globals = get_node("/root/globals")
+    var blocks = self.get_children()
+    var color = self._pick_random_color()
+    for i in range(4):
+        blocks[i].position = coords[i] * globals.TILE_SIZE
+        blocks[i].get_node('Sprite2D').set_modulate(color)
 
 #func _process(delta):
 #	pass
 
 func _pick_random_color():
-	var options = [
-		Color8(0, 200, 200),   # cyan
-		Color8(200, 200, 0),   # yellow
-		Color8(128, 0, 128),   # purple
-		Color8(0, 200, 0),     # green
-		Color8(200, 0, 0),     # red
-		Color8(65, 65, 240),     # blue
-		Color8(255, 127, 0),   # orange
-		Color8(150, 150, 150), # gray
-	]
-	return options[randi() % options.size()]
+    var options = [
+        Color8(0, 200, 200),   # cyan
+        Color8(200, 200, 0),   # yellow
+        Color8(128, 0, 128),   # purple
+        Color8(0, 200, 0),     # green
+        Color8(200, 0, 0),     # red
+        Color8(65, 65, 240),     # blue
+        Color8(255, 127, 0),   # orange
+        Color8(150, 150, 150), # gray
+    ]
+    return options[randi() % options.size()]
 
 func set_tpos(tpos):
-	self.tpos = tpos
+    self.tpos = tpos
 
 func get_tpos():
-	return self.tpos
+    return self.tpos
 
 func get_coords():
-	return self.coords
+    return self.coords
 
 func init(type, coords):
-	self.type = type
-	self.coords = coords
-	self.size = 0
-	if self.type == 'I':
-		self.size = 1
+    self.type = type
+    self.coords = coords
+    self.size = 0
+    if self.type == 'I':
+        self.size = 1
