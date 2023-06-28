1e29d69474eea214049a496db31a9615c9c683fa
Veren Damyanov <veren.damyanov@protonmail.com>
2023-06-28 23:32:01 +0300

    Fix some playfield boundary issues; rework the gravity system

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -3,5 +3,5 @@ extends Node
 const FIELD_X = 10 # x size of playfield in tiles
 const FIELD_Y = 24 # y size of playfield in tiles
 const OFFSET_X = 8 # x offset of playfield location in pixels
-const OFFSET_Y = 8 # y offset of playfield location in pixels
+const OFFSET_Y = -8 # y offset of playfield location in pixels
 const TILE_SIZE = 8 # size of one tile in pixels
--- scripts/playfield.gd
+++ scripts/playfield.gd
@@ -1,9 +1,12 @@
 extends Node2D
 
 var globals
-var matrix       # the board
-var elapsed      # time since last move
-var active_shape # currently active (falling) shape
+var matrix           # the board
+var active_shape     # currently active (falling) shape
+# note: current implementation can't support gravity higher than 1G
+# var gravity = 0.0156 # 0.0156G
+var gravity = 0.1 # 0.0156G
+var gravity_sum = 0  # elapsed gravity since last downwards move
 
 var shape_map = {
     'I': [Vector2(-1, 0), Vector2(0, 0), Vector2(1, 0), Vector2(2, 0)],
@@ -17,26 +20,27 @@ var shape_map = {
 
 func _ready():
     self.globals = get_node("/root/globals")
-    self.elapsed = 0
     self._init_matrix()
     self.active_shape = self._new_active_shape()
     self.add_child(self.active_shape)
 
 func _process(delta):
     self.get_node('Label').set_text("FPS %d" % Engine.get_frames_per_second())
-    elapsed += delta
+    gravity_sum += gravity
 
 #    var left = Input.is_action_pressed("left")
 #    var right = Input.is_action_pressed("right")
 #    var space = Input.is_action_pressed("space")
 #
 #    if left and not right:
-#        pass
+#        if self._is_active_shape_movable(-1, 0):
+#            self._move_active_shape(-1, 0)
 #    elif right and not left:
-#        pass
+#        if self._is_active_shape_movable(1, 0):
+#            self._move_active_shape(1, 0)
 
-    if elapsed >= 0.2:
-        elapsed = 0
+    if gravity_sum >= 1:
+        gravity_sum = 0
         if self._is_active_shape_movable(0, 1):
             self._move_active_shape(0, 1)
         else:
@@ -46,10 +50,10 @@ func _process(delta):
 
 func _new_active_shape():
     var shape = self._new_shape()
-    var x = globals.TILE_SIZE / 2 + globals.OFFSET_X + globals.TILE_SIZE * 4
-    var y = globals.TILE_SIZE / 2 + globals.OFFSET_Y + globals.TILE_SIZE * -2
+    var x = globals.TILE_SIZE / 2 + globals.TILE_SIZE * 5
+    var y = globals.TILE_SIZE / 2 + globals.TILE_SIZE * 0 + globals.OFFSET_Y
     shape.set_position(Vector2(x, y))
-    shape.set_tpos(Vector2(4, 0))  # FIXME: this position is temporary, should be changed (also above ^^)
+    shape.set_tpos(Vector2(5, 0))  # FIXME: this position is temporary, should be changed (also above ^^)
     return shape
 
 func _deactivate_current_shape():
@@ -75,21 +79,21 @@ func _move_active_shape(dx, dy):
     tpos.x += dx
     tpos.y += dy
     self.active_shape.set_tpos(tpos)
-    var x = globals.TILE_SIZE / 2 + globals.OFFSET_X + globals.TILE_SIZE * tpos.x
-    var y = globals.TILE_SIZE / 2 + globals.OFFSET_Y + globals.TILE_SIZE * tpos.y
+    var x = globals.TILE_SIZE / 2 + globals.TILE_SIZE * tpos.x
+    var y = globals.TILE_SIZE / 2 + globals.TILE_SIZE * tpos.y + globals.OFFSET_Y
     self.active_shape.set_position(Vector2(x, y))
 
 func _init_matrix():
     self.matrix = []
     for i in range(self.globals.FIELD_X+2):
         self.matrix.append([])
-        for j in range(self.globals.FIELD_Y):
+        for j in range(self.globals.FIELD_Y+2):
             if i == 0 or i == self.globals.FIELD_X+1:
                 self.matrix[i].append(1)
             else:
                 self.matrix[i].append(0)
         self.matrix[i].append(1)
-    # print(self.matrix)
+    print(self.matrix)
 
 func _randomize():
     var options = ['I', 'J', 'L', 'O', 'S', 'T', 'Z']
