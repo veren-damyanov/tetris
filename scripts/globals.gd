9805a1132d40727c7983ff46bf8d247b5edb1bd1
Veren Damyanov <veren.damyanov@protonmail.com>
2023-06-28 22:44:05 +0300

    Initial commit

---
+++ scripts/globals.gd
@@ -0,0 +1,7 @@
+extends Node
+
+const FIELD_X = 10 # x size of playfield in tiles
+const FIELD_Y = 24 # y size of playfield in tiles
+const OFFSET_X = 8 # x offset of playfield location in pixels
+const OFFSET_Y = 8 # y offset of playfield location in pixels
+const TILE_SIZE = 8 # size of one tile in pixels
---
+++ scripts/shape.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+var globals
+var type   # type of shape, e.g. J
+var coords # relative coords of the 4 blocks
+var size   # used for rotation
+var tpos  # position within field matrix
+
+func _ready():
+	globals = get_node("/root/globals")
+	var blocks = self.get_children()
+	var color = self._pick_random_color()
+	for i in range(4):
+		blocks[i].position = coords[i] * globals.TILE_SIZE
+		blocks[i].get_node('Sprite2D').set_modulate(color)
+
+#func _process(delta):
+#	pass
+
+func _pick_random_color():
+	var options = [
+		Color8(0, 200, 200),   # cyan
+		Color8(200, 200, 0),   # yellow
+		Color8(128, 0, 128),   # purple
+		Color8(0, 200, 0),     # green
+		Color8(200, 0, 0),     # red
+		Color8(65, 65, 240),     # blue
+		Color8(255, 127, 0),   # orange
+		Color8(150, 150, 150), # gray
+	]
+	return options[randi() % options.size()]
+
+func set_tpos(tpos):
+	self.tpos = tpos
+
+func get_tpos():
+	return self.tpos
+
+func get_coords():
+	return self.coords
+
+func init(type, coords):
+	self.type = type
+	self.coords = coords
+	self.size = 0
+	if self.type == 'I':
+		self.size = 1
