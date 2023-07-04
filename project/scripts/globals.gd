106d59938ff465d91e95280f56c133b2abe2d6ea
Veren Damyanov <veren.damyanov@protonmail.com>
2023-07-04 15:34:06 +0300

    Update the playfield image and add some new labels

--- project/scripts/globals.gd
+++ project/scripts/globals.gd
@@ -1,5 +1,5 @@
 extends Node
 
 const FIELD_X = 10 # x size of playfield in tiles
-const FIELD_Y = 24 # y size of playfield in tiles
+const FIELD_Y = 20 # y size of playfield in tiles
 const TILE_SIZE = 8 # size of one tile in pixels
