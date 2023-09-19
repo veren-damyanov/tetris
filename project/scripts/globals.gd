6db56c8b7dd34b24c8e5da5dd04ba5af5ada2569
Veren Damyanov <veren.damyanov@protonmail.com>
2023-09-20 01:17:19 +0300

    Fix platform detection

--- project/scripts/globals.gd
+++ project/scripts/globals.gd
@@ -5,11 +5,15 @@ const FIELD_Y = 20 # y size of playfield in tiles
 const TILE_SIZE = 8 # size of one tile in pixels
 const LAYOUT_NAMES = {0: "desktop", 1: "mobile"}
 const THEME_NAMES = {0: "classic", 1: "retro"}
+const ANDROID_KEYWORDS = ['android', 'linux aarch64', 'linux armv7l', 'linux i686']
+const IOS_KEYWORDS = ['iphone', 'ipad', 'ipod']
 
 enum LAYOUT {DESKTOP = 0, MOBILE = 1}
 enum THEME {CLASSIC = 0, RETRO = 1}
 
-var agent = JavaScriptBridge.eval("navigator.oscpu")
+var platform = JavaScriptBridge.eval("navigator.platform")
+var agent
+
 var current_layout = LAYOUT.DESKTOP
 var current_theme = THEME.CLASSIC
 var high_scores = []
@@ -25,6 +29,14 @@ func increment_theme():
         self.current_theme = 0
 
 func _ready():
-    if self.agent == null:
-        self.agent = 'dev'
-
+    if self.platform == null:
+        self.agent = 'dev(?)'
+    else:
+        self.platform = self.platform.to_lower()
+        self.agent = 'Desktop'
+        for word in ANDROID_KEYWORDS:
+            if word in self.platform:
+                self.agent = 'Android'
+        for word in IOS_KEYWORDS:
+            if word in self.platform:
+                self.agent = 'iOS'
